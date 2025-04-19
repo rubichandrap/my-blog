f0cfcf61c070d1cb68537a5282412f25253b1ad8
Rubi Chandraputra <rubichandrap@gmail.com>
2025-04-19 23:03:06 +0700

    chore: remove out folder since gh page already handle the publishing

--- out/_next/static/chunks/app/not-found-598013ac0aef09ba.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[345],{9289:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,7950,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[950,497,954,358],()=>s(9289)),_N_E=e.O()}]);
