4f746e51f9131bf850c32f2c0a0eb85f361c93b2
Rubi Chandraputra <rubichandrap@gmail.com>
2025-04-19 23:03:50 +0700

    chore: add basepath and assetprefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/my-blog",
+  assetPrefix: "/my-blog",
   eslint: {
     ignoreDuringBuilds: true,
   },
